fca40be28f8a4e72448982e0afb98742c72ae785
Mahesh Alladi <alladimaheshbabu333@gmail.com>
2019-03-09 10:03:43 +0530

    Update cricket.c

--- cricket.c
+++ cricket.c
@@ -110,11 +110,11 @@ void main()
 	}
 	else
 	{
-	    printf("    BATTING TEAM\n");
+	    printf("    BATTING TEAM \n");
 	    printf("SCORE : %d - %d",score,wicket);
 	    printf("\nplayer %d on strike\nplayer %d is on offstrike\n",player[0],player[1]);
 	}
-	printf("    BOWLING TEAM\n");
+	printf("    BOWLING TEAM \n");
 	printf("wickets taken: %d\n",wicket);
 	
 	
