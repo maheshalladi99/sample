fbc4f60647b6b5f41e0c0744952ac773d3035cc4
Mahesh Alladi <alladimaheshbabu333@gmail.com>
2019-03-14 12:25:52 +0530

    Update BEAUTY.C

--- BEAUTY.C
+++ BEAUTY.C
@@ -1,7 +1,7 @@
 void main( int argc,char **argv[])
 {
 	clrscr();
-	printf("argv = %d\n  ",argc);
+	printf("argv = %d\n ",argc);
 	printf("argv[0] = %s\n",argv[0]);
 	printf("argv[1] = %s\n",argv[1]);
 	getch();
